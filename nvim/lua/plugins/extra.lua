91a6528fc39af721002963526572b9336fd08e1d
Axel Sorenson <AxelPSorenson@gmail.com>
2024-02-19 00:28:47 -0800

    Added additional lazy extras, including Rust support. Also added extra games to NVim setup

---
+++ nvim/lua/plugins/extra.lua
@@ -0,0 +1,14 @@
+return {
+    {
+        "NStefan002/speedtyper.nvim",
+        cmd = "Speedtyper",
+        opts = {
+            -- your config
+        },
+    },
+    {
+        "NStefan002/2048.nvim",
+        cmd = "Play2048",
+        config = true,
+    },
+}
