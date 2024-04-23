603f20d4fc0ecbaf4bf1f791aeaeb8c90ee64259
Axel Sorenson <AxelPSorenson@gmail.com>
2024-04-23 01:40:17 -0700

    Additional Hyprland configuration (mostly theme stuff)

--- home-modules/hyprland-desktop/default.nix
+++ home-modules/hyprland-desktop/default.nix
@@ -13,5 +13,18 @@
     xdg.configFile.swayidle.source = ./swayidle;
     xdg.configFile.swaylock.source = ./swaylock;
     xdg.configFile.avizo.source = ./avizo;
+    xdg.configFile.xsettingsd.source = ./xsettingsd;
+    xdg.configFile.xfce4.source = ./xfce4;
+    xdg.configFile.wpaperd.source = ./wpaperd;
+    xdg.configFile.Thunar.source  = ./Thunar;
+    xdg.configFile."gtk-3.0".source  = ./gtk-3.0;
+    xdg.configFile."gtk-4.0".source  = ./gtk-4.0;
+    xdg.configFile.autostart.source  = ./autostart;
+    xdg.configFile.swappy.source  = ./swappy;
+    xdg.configFile.zellij.source  = ./zellij;    
+    xdg.configFile.Kvantum.source  = ./Kvantum;
+    home.file.".icons".source = ./.icons;
+    home.file.".gtkrc-2.0".source = ./.gtkrc-2.0;  
+    home.file.".face".source = ./.face;
   };
 }
--- nixos-modules/hyprland/default.nix
+++ nixos-modules/hyprland/default.nix
@@ -2,6 +2,7 @@
   imports = [
     ./hyprland.nix
     ./screen.nix
+    ./theme.nix
     ./services.nix
     ./display-manager.nix
   ];
