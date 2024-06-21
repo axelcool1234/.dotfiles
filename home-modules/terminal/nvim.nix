26365088174f252a3c1f1dc9d58eccd297be7cd2
Axel Sorenson <AxelPSorenson@gmail.com>
2024-06-21 14:16:20 -0700

    Nvim config now has cmp/ultisnips/lsp working

--- home-modules/terminal/nvim.nix
+++ home-modules/terminal/nvim.nix
@@ -13,23 +13,33 @@
       vimAlias = true;
       vimdiffAlias = true;
 
-      plugins = [
-
+      plugins = with pkgs.vimPlugins; [
         # Essentials
-        pkgs.vimPlugins.nvim-treesitter
-        pkgs.vimPlugins.nvim-treesitter-parsers.nix
-      	pkgs.vimPlugins.nvim-treesitter-parsers.vim
-	pkgs.vimPlugins.nvim-treesitter-parsers.vimdoc
-      	pkgs.vimPlugins.nvim-treesitter-parsers.toml
-      	pkgs.vimPlugins.nvim-treesitter-parsers.rust
-      	pkgs.vimPlugins.nvim-treesitter-parsers.cpp
-      	pkgs.vimPlugins.nvim-treesitter-parsers.python
-      	pkgs.vimPlugins.telescope-nvim
-      	pkgs.vimPlugins.undotree
+        nvim-treesitter
+        nvim-treesitter-parsers.nix
+      	nvim-treesitter-parsers.vim
+      	nvim-treesitter-parsers.vimdoc
+      	nvim-treesitter-parsers.toml
+      	nvim-treesitter-parsers.rust
+      	nvim-treesitter-parsers.cpp
+      	nvim-treesitter-parsers.python
+      	telescope-nvim
+      	undotree
+
+        # LSP
+        nvim-lspconfig
+        nvim-cmp
+        cmp-nvim-lsp       # Completion source for nvim builtin lsp
+        cmp-buffer         # Completion source for nvim-cmp
+        cmp-path           # Completion source for file system paths
+        cmp-cmdline        # Completion source for nvim's command line
+        cmp-nvim-ultisnips # Completion source for ultisnips
+        ultisnips
+        friendly-snippets
 
         # Themes
-        pkgs.vimPlugins.catppuccin-nvim
-        pkgs.vimPlugins.tokyonight-nvim      
+        catppuccin-nvim
+        tokyonight-nvim      
       ];
 
     };
