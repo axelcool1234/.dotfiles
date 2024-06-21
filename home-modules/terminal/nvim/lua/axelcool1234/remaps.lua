ff8f8282488cb5337e88d7e404d4a49727e74b83
Axel Sorenson <AxelPSorenson@gmail.com>
2024-06-21 14:31:26 -0700

    More remaps for nvim that I commonly use in helix

--- home-modules/terminal/nvim/lua/axelcool1234/remaps.lua
+++ home-modules/terminal/nvim/lua/axelcool1234/remaps.lua
@@ -1,1 +1,12 @@
 vim.g.mapleader = " "
+
+-- Paste from system clipboard
+vim.api.nvim_set_keymap('n', '<leader>p', '"+p', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<leader>p', '"+p', { noremap = true, silent = true })
+
+-- Yank to system clipboard
+vim.api.nvim_set_keymap('n', '<leader>y', '"+y', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<leader>y', '"+y', { noremap = true, silent = true })
+
+-- Replace symbol
+vim.api.nvim_set_keymap('n', '<leader>r', '<cmd>lua vim.lsp.buf.rename()<CR>', { noremap = true, silent = true })
