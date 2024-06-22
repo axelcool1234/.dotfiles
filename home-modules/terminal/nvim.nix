6f3130c58d4705f62a10e12fd1a8a4bd9f84d785
Axel Sorenson <AxelPSorenson@gmail.com>
2024-06-21 23:49:32 -0700

    Moved all remappings to which-key (nvim config)

--- home-modules/terminal/nvim.nix
+++ home-modules/terminal/nvim.nix
@@ -37,6 +37,9 @@
         ultisnips
         friendly-snippets
 
+        # Editor
+        which-key-nvim
+
         # Themes
         catppuccin-nvim
         tokyonight-nvim      
--- home-modules/terminal/nvim/lua/axelcool1234/remaps.lua
+++ home-modules/terminal/nvim/lua/axelcool1234/remaps.lua
@@ -1,52 +1,7 @@
+-- Leader Key
 vim.g.mapleader = " "
 
--- Paste from system clipboard
-vim.api.nvim_set_keymap('n', '<leader>p', '"+p', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('v', '<leader>p', '"+p', { noremap = true, silent = true })
-
--- Yank to system clipboard
-vim.api.nvim_set_keymap('n', '<leader>y', '"+y', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('v', '<leader>y', '"+y', { noremap = true, silent = true })
-
--- CTRL+D / CTRL+U keeps cursor in the middle
-vim.api.nvim_set_keymap('n', '<C-d>', '<C-d>zz', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-u>', '<C-u>zz', { noremap = true, silent = true })
-
--- Replace symbol
-vim.api.nvim_set_keymap('n', '<leader>r', '<cmd>lua vim.lsp.buf.rename()<CR>', { noremap = true, silent = true })
-
--- Code Action
-vim.api.nvim_set_keymap('n', '<leader>a', '<cmd>lua vim.lsp.buf.code_action()<CR>', { noremap = true, silent = true })
-
--- Move through diagnostic
-vim.api.nvim_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', { noremap = true, silent = true })
-
--- Goto declaration/definition/implementation/references
-vim.api.nvim_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', { noremap = true, silent = true })
-
--- Diagnostics Picker
-vim.api.nvim_set_keymap('n', '<leader> ', '<cmd>lua vim.lsp.buf.hover()<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>s', '<cmd>lua vim.lsp.buf.signature_help()<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>d', '<cmd>lua vim.diagnostic.open_float()<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', { noremap = true, silent = true })
-
--- Key mappings for nvim-cmp (completion-nvim)
-vim.api.nvim_set_keymap('i', '<C-p>', [[<cmd>lua require('cmp').select_prev_item()<CR>]], { noremap = true, silent = true })
-vim.api.nvim_set_keymap('i', '<C-n>', [[<cmd>lua require('cmp').select_next_item()<CR>]], { noremap = true, silent = true })
--- vim.api.nvim_set_keymap('i', '<C-space>', [[<cmd>lua require('cmp').complete()<CR>]], { noremap = true, silent = true })
-vim.api.nvim_set_keymap('i', '<C-e>', [[<cmd>lua require('cmp').close()<CR>]], { noremap = true, silent = true })
--- vim.api.nvim_set_keymap('i', '<tab>', [[<cmd>lua require('cmp').confirm({ select = true })<CR>]], { noremap = true, silent = true })
-
--- Ultisnips keymappings
-vim.g.UltiSnipsExpandTrigger = "<tab>"
-vim.g.UltiSnipsJumpForwardTrigger = "<c-j>"
-vim.g.UltiSnipsJumpBackwardTrigger = "<c-k>"
-
--- Telescope keymappings
+-- Helpers
 _G.find_files_in_git_root = function()
     local root = vim.fn.system("git rev-parse --show-toplevel"):gsub("\n", "")
     if vim.v.shell_error == 0 then
@@ -65,5 +20,57 @@ _G.live_grep_in_git_root = function()
     end
 end
 
-vim.api.nvim_set_keymap('n', '<leader>f', '<cmd>lua find_files_in_git_root()<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>/', '<cmd>lua live_grep_in_git_root()<CR>', { noremap = true, silent = true })
+-- Which-Key labels
+require("which-key").register({
+    -- Telescope keymappings
+    ["<leader>f"] = { "<cmd>lua find_files_in_git_root()<CR>", "Find Files", mode = "n", noremap = true, silent = true },
+    ["<leader>/"] = { "<cmd>lua live_grep_in_git_root()<CR>", "Live Grep", mode = "n", noremap = true, silent = true },
+
+    -- Paste from system clipboard
+    ["<leader>p"] = { '"+p', "Paste from system clipboard", noremap = true, silent = true },
+    ["v<leader>p"] = { '"+p', "Paste from system clipboard", noremap = true, silent = true },
+
+    -- Yank to system clipboard
+    ["<leader>y"] = { '"+y', "Yank to system clipboard", noremap = true, silent = true },
+    ["v<leader>y"] = { '"+y', "Yank to system clipboard", noremap = true, silent = true },
+    
+    -- CTRL+D / CTRL+U keeps cursor in the middle
+    ["<C-d>"] = { "<C-d>zz", "CTRL+D keeps cursor in the middle", mode = "n", noremap = true, silent = true },
+    ["<C-u>"] = { "<C-u>zz", "CTRL+U keeps cursor in the middle", mode = "n", noremap = true, silent = true },
+
+    -- Replace symbol
+    ["<leader>r"] = { "<cmd>lua vim.lsp.buf.rename()<CR>", "Replace symbol", mode = "n", noremap = true, silent = true },
+
+    -- Code Action
+    ["<leader>a"] = { "<cmd>lua vim.lsp.buf.code_action()<CR>", "Code Action", mode = "n", noremap = true, silent = true },
+
+    -- Move through diagnostic
+    ["[d"] = { "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", "Move through diagnostic (prev)", noremap = true, silent = true, mode = "n" },
+    ["]d"] = { "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", "Move through diagnostic (next)", noremap = true, silent = true },
+
+    -- Goto declaration/definition/implementation/references
+    ["gD"] = { "<cmd>lua vim.lsp.buf.declaration()<CR>", "Goto declaration", noremap = true, silent = true },
+    ["gd"] = { "<cmd>lua vim.lsp.buf.definition()<CR>", "Goto definition", noremap = true, silent = true },
+    ["gi"] = { "<cmd>lua vim.lsp.buf.implementation()<CR>", "Goto implementation", noremap = true, silent = true },
+    ["gr"] = { "<cmd>lua vim.lsp.buf.references()<CR>", "Goto references", noremap = true, silent = true },
+
+    -- Diagnostics 
+    ["<leader> "] = { "<cmd>lua vim.lsp.buf.hover()<CR>", "Hover", noremap = true, silent = true },
+    ["<leader>s"] = { "<cmd>lua vim.lsp.buf.signature_help()<CR>", "Signature Help", noremap = true, silent = true },
+    ["<leader>d"] = { "<cmd>lua vim.diagnostic.open_float()<CR>", "Open Diagnostic float", noremap = true, silent = true },
+    ["<leader>q"] = { "<cmd>lua vim.diagnostic.setloclist()<CR>", "Set Diagnostic loclist", noremap = true, silent = true },
+
+    -- Key mappings for nvim-cmp (completion-nvim)
+    ["<C-p>"] = { "<cmd>lua require('cmp').select_prev_item()<CR>", "Completion: Previous item", mode = "i", noremap = true, silent = true },
+    ["<C-n>"] = { "<cmd>lua require('cmp').select_next_item()<CR>", "Completion: Next item", mode = "i", noremap = true, silent = true },
+    ["<C-e>"] = { "<cmd>lua require('cmp').close()<CR>", "Completion: Close", mode = "i", noremap = true, silent = true },
+
+    -- Key mappings for undotree
+    ["<leader>u"] = { "<cmd>lua vim.cmd.UndotreeToggle()<CR>", "Toggle Undotree", mode = "n", noremap = true, silent = true },
+
+    -- Ultisnips keymappings
+    ["<tab>"] = { "<cmd>lua UltiSnips#ExpandSnippet()<CR>", "UltiSnips: Expand Trigger", noremap = true, silent = true },
+    ["<c-j>"] = { "<cmd>lua UltiSnips#JumpForwards()<CR>", "UltiSnips: Jump Forward", noremap = true, silent = true },
+    ["<c-k>"] = { "<cmd>lua UltiSnips#JumpBackwards()<CR>", "UltiSnips: Jump Backward", noremap = true, silent = true },
+})
+ 
