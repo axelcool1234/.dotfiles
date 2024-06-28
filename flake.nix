122c96e9c943b55632dd52f00f9664e46f1abd93
Axel Sorenson <AxelPSorenson@gmail.com>
2024-06-28 02:01:02 -0700

    Added a new host (Foundry VTT server)

--- flake.nix
+++ flake.nix
@@ -12,15 +12,17 @@
       # See: https://github.com/wez/wezterm/issues/5255
       hyprland.url = "github:hyprwm/Hyprland/c198d744b77f272c2fc187eb6d431580a99ab6c3";
       spicetify-nix.url = "github:the-argus/spicetify-nix";
+      foundryvtt.url = "github:reckenrode/nix-foundryvtt";
     };
 
 	outputs = { self, nixpkgs, home-manager, ... }@inputs:
 	let
 	  system = "x86_64-linux";
-		pkgs = nixpkgs.legacyPackages.${system};
+	  pkgs = nixpkgs.legacyPackages.${system};
 	in
 	{
-	  # List of system configurations
+
+	# List of system configurations
     nixosConfigurations = {
       # Default config
       default = nixpkgs.lib.nixosSystem {
@@ -31,9 +33,19 @@
           ./nixos-modules
         ];
       };
+
+      # Foundry VTT Server
+      foundry = nixpkgs.lib.nixosSystem {
+          inherit system;
+          specialArgs = { inherit inputs; };
+          modules = [
+            ./hosts/foundry/configuration.nix
+            inputs.foundryvtt.nixosModules.foundryvtt
+          ];
+      };
     };
 
-		# List of user configurations
+    # List of user configurations
     homeConfigurations = {
       # Main user
       axelcool1234 = home-manager.lib.homeManagerConfiguration {
