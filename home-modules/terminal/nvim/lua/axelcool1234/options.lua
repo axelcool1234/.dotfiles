a33187ce2c3e90b45db49c014911b3d2f37620c5
Axel Sorenson <AxelPSorenson@gmail.com>
2024-09-03 15:29:24 -0700

    Fixed rustaceanvim config?

--- home-modules/terminal/nvim/lua/axelcool1234/options.lua
+++ home-modules/terminal/nvim/lua/axelcool1234/options.lua
@@ -37,3 +37,6 @@ vim.g.vimtex_view_method = 'zathura'
 
 -- Configure forward search
 vim.g.vimtex_view_general_options = '--synctex-forward @line:@col:@pdf %p'
+
+-- Rustaceanvim
+vim.g.rustaceanvim.tools.test_executor = 'background'
--- home-modules/terminal/nvim/lua/axelcool1234/rustaceanvim.lua
+++
@@ -1,1 +0,0 @@
-vim.g.rustaceanvim.tools.test_executor = 'background'
