a76c5f2000f3ce43c10ca5e5e85dc32288db1958
Axel Sorenson <AxelPSorenson@gmail.com>
2024-09-04 13:45:03 -0700

    Noise Supression plugin for Pipewire

--- nixos-modules/sound.nix
+++ nixos-modules/sound.nix
@@ -21,6 +21,41 @@
       # use the example session manager (no others are packaged yet so this is enabled by default,
       # no need to redefine it in your config for now)
       #media-session.enable = true;
+
+      # This adds a noise suppression plugin to pipewire so I can actually talk on Discord. 
+      extraLv2Packages = [ pkgs.rnnoise-plugin ];
+      configPackages = [
+        (pkgs.writeTextDir "share/pipewire/pipewire.conf.d/20-rnnoise.conf" ''
+          context.modules = [
+          {   name = libpipewire-module-filter-chain
+              args = {
+                  node.description = "Noise Canceling source"
+                  media.name = "Noise Canceling source"
+                  filter.graph = {
+                      nodes = [
+                          {
+                              type = lv2
+                              name = rnnoise
+                              plugin = "https://github.com/werman/noise-suppression-for-voice#stereo"
+                              label = noise_suppressor_stereo
+                              control = {
+                              }
+                          }
+                      ]
+                  }
+                  capture.props = {
+                      node.name =  "capture.rnnoise_source"
+                      node.passive = true
+                  }
+                  playback.props = {
+                      node.name =  "rnnoise_source"
+                      media.class = Audio/Source
+                  }
+              }
+          }
+          ]
+        '')
+      ];
     };
     environment.systemPackages = with pkgs; lib.mkIf config.sound-control.enable [
       pamixer
