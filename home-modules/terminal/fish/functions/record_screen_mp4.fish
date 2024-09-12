12539b324a8e4c633e083154da83669296d4fa72
Axel Sorenson <AxelPSorenson@gmail.com>
2024-09-12 02:11:38 -0700

    MP4s are saved to Pictures/Records/ Now

--- home-modules/terminal/fish/functions/record_screen_mp4.fish
+++ home-modules/terminal/fish/functions/record_screen_mp4.fish
@@ -8,8 +8,8 @@ function record_screen_mp4
         if not [ -z $geometry ]
             set record_name $(echo "recrod-$(date +"%Y-%m-%d--%H:%M:%S")")
             dunstify -i ~/.config/fish/icons/camera_mp4_icon.png -r $(cd ~/Videos/Records/ && ls -1 | wc -l) "Recording Started  (MP4)" -t 2000
-            wl-screenrec -g "$geometry" -f "$HOME/Videos/Records/$record_name.mp4"
-            wl-copy -t text/uri-list file://$HOME/Videos/Records/$record_name.mp4\n
+            wl-screenrec -g "$geometry" -f "$HOME/Pictures/Records/$record_name.mp4"
+            wl-copy -t text/uri-list file://$HOME/Pictures/Records/$record_name.mp4\n
             dunstify -i ~/.config/fish/icons/camera_mp4_icon.png -r $(cd ~/Videos/Records/ && ls -1 | wc -l) "Recording Stopped 󰙧 (MP4)" -t 2000
         end
     end
