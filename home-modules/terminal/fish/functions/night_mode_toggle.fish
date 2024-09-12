61009d16e9c26698b43084b0f4a36960777fbc82
Axel Sorenson <AxelPSorenson@gmail.com>
2024-09-12 05:47:37 -0700

    night-mode works (for now)

--- home-modules/terminal/fish/functions/night_mode_toggle.fish
+++ home-modules/terminal/fish/functions/night_mode_toggle.fish
@@ -4,6 +4,7 @@ function night_mode_toggle
     if pgrep $target_process > /dev/null
         killall -s SIGINT .gammastep-wrap
     else
-        gammastep
+        # BUG: geopositioning doesn't work, which is why I set the lat:lon here.
+        gammastep -l 33.6424:-117.8417 # UCI lat:long coords     
     end
 end
