fcf10f4cf196a36a25518925ed68aec48fb7237a
Axel Sorenson <AxelPSorenson@gmail.com>
2024-09-21 01:43:03 -0700

    Improvement on Wezterm bindings (may change depending on how this feels)

--- home-modules/terminal/wezterm/wezterm.lua
+++ home-modules/terminal/wezterm/wezterm.lua
@@ -2,7 +2,7 @@
 local wezterm = require 'wezterm'
 local act = wezterm.action
 -- This will hold the configuration.
-return { 
+return {
   -- max_fps = 165, 
   enable_wayland = true;
   color_scheme = 'Tokyo Night',
@@ -11,39 +11,42 @@ return {
   leader = { key = 'b', mods = 'CTRL'},
    -- disable_default_key_bindings = true,
   keys = {
-    { key = "b", mods = "LEADER|CTRL",  action=wezterm.action{SendString="\x01"}},
-    { key = "-", mods = "LEADER",       action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}},
-    { key = "\\",mods = "LEADER",       action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}}},
-    { key = "s", mods = "LEADER",       action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}},
-    { key = "v", mods = "LEADER",       action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}}},
-    { key = "o", mods = "LEADER",       action="TogglePaneZoomState" },
-    { key = "z", mods = "LEADER",       action="TogglePaneZoomState" },
-    { key = "c", mods = "LEADER",       action=wezterm.action{SpawnTab="CurrentPaneDomain"}},
-    { key = "h", mods = "CTRL",	        action=wezterm.action{ActivatePaneDirection="Left"}},
-    { key = "j", mods = "CTRL",	        action=wezterm.action{ActivatePaneDirection="Down"}},
-    { key = "k", mods = "CTRL",	        action=wezterm.action{ActivatePaneDirection="Up"}},
-    { key = "l", mods = "CTRL",	        action=wezterm.action{ActivatePaneDirection="Right"}},
-    { key = "h", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Left"}},
-    { key = "j", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Down"}},
-    { key = "k", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Up"}},
-    { key = "l", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Right"}},
-    { key = "H", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Left", 5}}},
-    { key = "J", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Down", 5}}},
-    { key = "K", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Up", 5}}},
-    { key = "L", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Right", 5}}},
-    { key = "1", mods = "LEADER",       action=wezterm.action{ActivateTab=0}},
-    { key = "2", mods = "LEADER",       action=wezterm.action{ActivateTab=1}},
-    { key = "3", mods = "LEADER",       action=wezterm.action{ActivateTab=2}},
-    { key = "4", mods = "LEADER",       action=wezterm.action{ActivateTab=3}},
-    { key = "5", mods = "LEADER",       action=wezterm.action{ActivateTab=4}},
-    { key = "6", mods = "LEADER",       action=wezterm.action{ActivateTab=5}},
-    { key = "7", mods = "LEADER",       action=wezterm.action{ActivateTab=6}},
-    { key = "8", mods = "LEADER",       action=wezterm.action{ActivateTab=7}},
-    { key = "9", mods = "LEADER",       action=wezterm.action{ActivateTab=8}},
-    { key = "&", mods = "LEADER|SHIFT", action=wezterm.action{CloseCurrentTab={confirm=true}}},
-    { key = "d", mods = "LEADER",       action=wezterm.action{CloseCurrentPane={confirm=true}}},
-    { key = "x", mods = "LEADER",       action=wezterm.action{CloseCurrentPane={confirm=true}}},
-    { key = ";", mods = "CTRL",         action=act.SendKey{key="UpArrow"}},
-    { key = "'", mods = "CTRL",         action=act.SendKey{key="DownArrow"}},
+    { key = "b", mods = "LEADER|CTRL", action=wezterm.action{SendString="\x01"}},
+
+    -- Toggle Pane Fullscreen
+    { key = "b", mods = "LEADER",      action="TogglePaneZoomState" },
+
+    -- New Panes
+    { key = "h", mods = "CTRL",        action = wezterm.action.SplitPane { direction = "Left", size = { Percent = 50 } } },
+    { key = "j", mods = "CTRL",        action = wezterm.action.SplitPane { direction = "Down", size = { Percent = 50 } } },
+    { key = "k", mods = "CTRL",        action = wezterm.action.SplitPane { direction = "Up", size = { Percent = 50 } } },
+    { key = "l", mods = "CTRL",        action = wezterm.action.SplitPane { direction = "Right", size = { Percent = 50 } } },
+
+    -- Switch Panes
+    { key = "h", mods = "SUPER|SHIFT", action=wezterm.action{ActivatePaneDirection="Left"}},
+    { key = "j", mods = "SUPER|SHIFT", action=wezterm.action{ActivatePaneDirection="Down"}},
+    { key = "k", mods = "SUPER|SHIFT", action=wezterm.action{ActivatePaneDirection="Up"}},
+    { key = "l", mods = "SUPER|SHIFT", action=wezterm.action{ActivatePaneDirection="Right"}},
+
+    -- Alter Pane Sizes
+    { key = "H", mods = "CTRL|SHIFT",  action=wezterm.action{AdjustPaneSize={"Left", 5}}},
+    { key = "J", mods = "CTRL|SHIFT",  action=wezterm.action{AdjustPaneSize={"Down", 5}}},
+    { key = "K", mods = "CTRL|SHIFT",  action=wezterm.action{AdjustPaneSize={"Up", 5}}},
+    { key = "L", mods = "CTRL|SHIFT",  action=wezterm.action{AdjustPaneSize={"Right", 5}}},
+
+    -- Switching tabs
+    { key = "1", mods = "LEADER",      action=wezterm.action{ActivateTab=0}},
+    { key = "2", mods = "LEADER",      action=wezterm.action{ActivateTab=1}},
+    { key = "3", mods = "LEADER",      action=wezterm.action{ActivateTab=2}},
+    { key = "4", mods = "LEADER",      action=wezterm.action{ActivateTab=3}},
+    { key = "5", mods = "LEADER",      action=wezterm.action{ActivateTab=4}},
+    { key = "6", mods = "LEADER",      action=wezterm.action{ActivateTab=5}},
+    { key = "7", mods = "LEADER",      action=wezterm.action{ActivateTab=6}},
+    { key = "8", mods = "LEADER",      action=wezterm.action{ActivateTab=7}},
+    { key = "9", mods = "LEADER",      action=wezterm.action{ActivateTab=8}},
+
+    -- Previous terminal commands
+    { key = ";", mods = "CTRL",        action=act.SendKey{key="UpArrow"}},
+    { key = "'", mods = "CTRL",        action=act.SendKey{key="DownArrow"}},
   },
 }
