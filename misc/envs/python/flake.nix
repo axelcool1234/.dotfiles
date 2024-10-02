a9380bfee13c3840863e07ae2ba02c1047147a5e
Axel Sorenson <AxelPSorenson@gmail.com>
2024-10-02 15:45:44 -0700

    Added Python environment

---
+++ misc/envs/python/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Python Environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+  }; 
+
+  outputs = { self, nixpkgs, ... }: 
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in
+  {
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      nativeBuildInputs = with pkgs; [
+        python3
+      ];
+      propagatedBuildInputs = with pkgs.python311Packages; [
+        pip
+      ];
+      shellHook = ''
+        export HELIX_RUNTIME="$PWD/runtime"
+      '';
+    };
+  };
+}
