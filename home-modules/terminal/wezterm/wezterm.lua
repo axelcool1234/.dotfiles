07dafd87f8409075b44300da904bbe8b48c5d4b1
Axel Sorenson <AxelPSorenson@gmail.com>
2025-05-31 20:51:17 -0700

    Added a custom "Editor Mode" to Wezterm

--- home-modules/terminal/wezterm/wezterm.lua
+++ home-modules/terminal/wezterm/wezterm.lua
@@ -1,5 +1,7 @@
 -- Pull in the wezterm API
 local wezterm = require 'wezterm'
+local io = require 'io';
+local os = require 'os';
 local act = wezterm.action
 
 -- Custom Actions
@@ -40,6 +42,39 @@ wezterm.on('minimize-toggle', function(window, pane)
       )
       helper_pane_open = true
     end
+end)
+
+
+wezterm.on("trigger-editor-with-scrollback", function(window, pane)
+  -- Retrieve the current viewport's text.
+  -- Pass an optional number of lines (eg: 2000) to retrieve
+  -- that number of lines starting from the bottom of the viewport
+  local scrollback = pane:get_lines_as_text();
+
+  -- Create a temporary file to pass to vim
+  local name = os.tmpname();
+  local f = io.open(name, "w+");
+  f:write(scrollback);
+  f:flush();
+  f:close();
+
+  -- Use $EDITOR or default to 'vim' if $EDITOR is not set
+  -- FIXME: Should be dynamic, but it seems to open in Nano despite EDITOR being set to something else.
+  -- local editor = os.getenv("EDITOR") or "vim"
+  local editor = "nvim"
+
+
+  -- Open a new tab running vim and tell it to open the file
+  window:perform_action(wezterm.action{SpawnCommandInNewTab={
+    args={editor, name}}
+  }, pane)
+
+  -- wait "enough" time for editor to read the file before we remove it.
+  -- The window creation and process spawn are asynchronous
+  -- wrt. running this script and are not awaitable, so we just pick
+  -- a number.
+  wezterm.sleep_ms(1000);
+  os.remove(name);
 end)
 
 -- This will hold the configuration.
@@ -92,5 +127,8 @@ return {
     -- Previous terminal commands
     { key = ";", mods = "CTRL",        action=act.SendKey{key="UpArrow"}},
     { key = "'", mods = "CTRL",        action=act.SendKey{key="DownArrow"}},
+
+    -- Editor Mode
+    { key = "E", mods = "CTRL|SHIFT",    action=wezterm.action{EmitEvent="trigger-editor-with-scrollback"}},
   },
 }
