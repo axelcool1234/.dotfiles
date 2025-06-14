8cda665b22bf65dc92decec75336814121e951d9
Axel Sorenson <AxelPSorenson@gmail.com>
2025-06-13 19:43:02 -0700

    Updated system, turned off Nvidia, added an external monitor

--- hosts/Legion-Laptop/drivers.nix
+++ hosts/Legion-Laptop/drivers.nix
@@ -9,26 +9,26 @@
   services.fstrim.enable = lib.mkDefault true;
 
   # --- Disable NVidia dGPU Settings --- #
-  # boot.extraModprobeConfig = ''
-  #   blacklist nouveau
-  #   options nouveau modeset=0
-  # '';
+  boot.extraModprobeConfig = ''
+    blacklist nouveau
+    options nouveau modeset=0
+  '';
 
-  # services.udev.extraRules = ''
-  #   # Remove NVIDIA USB xHCI Host Controller devices, if present
-  #   ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{power/control}="auto", ATTR{remove}="1"
-  #   # Remove NVIDIA USB Type-C UCSI devices, if present
-  #   ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{power/control}="auto", ATTR{remove}="1"
-  #   # Remove NVIDIA Audio devices, if present
-  #   ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{power/control}="auto", ATTR{remove}="1"
-  #   # Remove NVIDIA VGA/3D controller devices
-  #   ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", ATTR{power/control}="auto", ATTR{remove}="1"
-  # '';
+  services.udev.extraRules = ''
+    # Remove NVIDIA USB xHCI Host Controller devices, if present
+    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{power/control}="auto", ATTR{remove}="1"
+    # Remove NVIDIA USB Type-C UCSI devices, if present
+    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{power/control}="auto", ATTR{remove}="1"
+    # Remove NVIDIA Audio devices, if present
+    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{power/control}="auto", ATTR{remove}="1"
+    # Remove NVIDIA VGA/3D controller devices
+    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", ATTR{power/control}="auto", ATTR{remove}="1"
+  '';
 
   # boot.blacklistedKernelModules = [ "nouveau" "nvidia" "nvidia_drm" "nvidia_modeset" ];
 
   # NOTE: If you want the dGPU enabled, you should still blacklist nouveau drivers
-  boot.blacklistedKernelModules=["nouveau"];
+  # boot.blacklistedKernelModules=["nouveau"];
 
   # --- Boot Settings --- #
   boot.kernelPackages = pkgs.linuxKernel.packages.linux_zen;
@@ -80,57 +80,57 @@
 
   # --- Nvidia Settings --- #
   # Load nvidia driver for Xorg and Wayland
-  services.xserver.videoDrivers = ["nvidia"];
+  # services.xserver.videoDrivers = ["nvidia"];
 
   # Enable access to nvidia from containers (Docker, Podman)
   # hardware.nvidia-container-toolkit.enable = true;
 
-  hardware.nvidia = {
-    # Modesetting is required.
-    modesetting.enable = true;
+  # hardware.nvidia = {
+  #   # Modesetting is required.
+  #   modesetting.enable = true;
 
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    # Enable this if you have graphical corruption issues or application crashes after waking
-    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
-    # of just the bare essentials.
-    powerManagement.enable = true;
+  #   # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+  #   # Enable this if you have graphical corruption issues or application crashes after waking
+  #   # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
+  #   # of just the bare essentials.
+  #   powerManagement.enable = true;
 
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = true;
+  #   # Fine-grained power management. Turns off GPU when not in use.
+  #   # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+  #   powerManagement.finegrained = true;
 
-    # Dynamic Boost. It is a technology found in NVIDIA Max-Q design laptops with RTX GPUs.
-    # It intelligently and automatically shifts power between
-    # the CPU and GPU in real-time based on the workload of your game or application.
-    dynamicBoost.enable = true;
+  #   # Dynamic Boost. It is a technology found in NVIDIA Max-Q design laptops with RTX GPUs.
+  #   # It intelligently and automatically shifts power between
+  #   # the CPU and GPU in real-time based on the workload of your game or application.
+  #   dynamicBoost.enable = true;
 
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of 
-    # supported GPUs is at:
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
-    # Only available from driver 515.43.04+
-    # Currently alpha-quality/buggy, so false is currently the recommended setting.
-    open = true;
+  #   # Use the NVidia open source kernel module (not to be confused with the
+  #   # independent third-party "nouveau" open source driver).
+  #   # Support is limited to the Turing and later architectures. Full list of 
+  #   # supported GPUs is at:
+  #   # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+  #   # Only available from driver 515.43.04+
+  #   # Currently alpha-quality/buggy, so false is currently the recommended setting.
+  #   open = true;
 
-    # Enable the Nvidia settings menu,
-  	# accessible via `nvidia-settings`.
-    nvidiaSettings = true;
+  #   # Enable the Nvidia settings menu,
+  # 	# accessible via `nvidia-settings`.
+  #   nvidiaSettings = true;
 
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.production;
+  #   # Optionally, you may need to select the appropriate driver version for your specific GPU.
+  #   package = config.boot.kernelPackages.nvidiaPackages.production;
 
-    # Offload Mode
-    prime = { /* also from nixos-hardware/common/gpu/nvidia/prime.nix */
-        offload = {
-            enable = true;
-            enableOffloadCmd = true;
-        };
-        # Make sure to use the correct Bus ID values for your system!
-        intelBusId = "PCI:00:02:0";
-        nvidiaBusId = "PCI:01:00:0";
-    };
-  };
+  #   # Offload Mode
+  #   prime = { /* also from nixos-hardware/common/gpu/nvidia/prime.nix */
+  #       offload = {
+  #           enable = true;
+  #           enableOffloadCmd = true;
+  #       };
+  #       # Make sure to use the correct Bus ID values for your system!
+  #       intelBusId = "PCI:00:02:0";
+  #       nvidiaBusId = "PCI:01:00:0";
+  #   };
+  # };
 
   # NixOS specialization named 'nvidia-sync'. Provides the ability
   # to switch the Nvidia Optimus Prime profile
