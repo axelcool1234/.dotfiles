deff829861c51d5314c014f3ede75a58c9bc2b94
Axel Sorenson <AxelPSorenson@gmail.com>
2025-06-24 13:08:48 -0700

    Turn onTurn on Nvidia, add Quickshell flake for experimentation

---
+++ misc/envs/quickshell/flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Quickshell Environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    quickshell = {
+      # add ?ref=<tag> to track a tag
+      url = "git+https://git.outfoxxed.me/outfoxxed/quickshell";
+
+      # THIS IS IMPORTANT
+      # Mismatched system dependencies will lead to crashes and other issues.
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    quickshell,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+        with pkgs; {
+          devShells.default = mkShell {
+            packages = [
+              quickshell.packages.${pkgs.system}.default
+            ];
+          };
+        }
+    );
+}
