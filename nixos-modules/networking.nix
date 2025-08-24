f456048d0053af545434216373baef6fdc291d05
Axel Sorenson <AxelPSorenson@gmail.com>
2025-08-24 16:19:24 -0600

    Turn off port for FoundryVTT

--- nixos-modules/networking.nix
+++ nixos-modules/networking.nix
@@ -5,12 +5,12 @@
   networking.hostName = "nixos"; # Define your hostname.
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  # Used for Foundry VTT 
-  networking.firewall.allowedTCPPorts = [ 30000 ];
+  # Used for Foundry VTT
+  # networking.firewall.allowedTCPPorts = [ 30000 ];
 }
