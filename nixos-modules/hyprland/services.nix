9d401ac30afb0a24c54b37f9d387657bfa8b5727
Axel Sorenson <AxelPSorenson@gmail.com>
2025-09-26 19:34:54 -0600

    Add automount rules for keyboard

--- nixos-modules/hyprland/services.nix
+++ nixos-modules/hyprland/services.nix
@@ -1,4 +1,9 @@
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 {
   config = lib.mkIf config.hyprland.enable {
@@ -24,51 +29,63 @@
     services.mpd.enable = true;
     programs.thunar.enable = true;
     programs.xfconf.enable = true;
-    services.tumbler.enable = true; 
+    services.tumbler.enable = true;
     services.fwupd.enable = true;
     services.auto-cpufreq.enable = true;
     # services.udev.packages = with pkgs; [ gnome.gnome-settings-daemon ];
 
+    # Automount rules (just configured for my split keyboard right now)
+    services.udev.extraRules = ''
+      # Automount RP2040 bootloader volume (RPI-RP2)
+      SUBSYSTEMS=="usb", SUBSYSTEM=="block", ACTION=="add", ENV{ID_FS_LABEL}=="RPI-RP2", RUN+="${pkgs.writeShellScript "udev-rpi-mount.sh" ''
+        ${pkgs.systemd}/bin/systemd-mount --no-block --automount=yes --collect --options=X-mount.mkdir "$DEVNAME" "/run/media/$ID_FS_LABEL"
+      ''}"
+      SUBSYSTEMS=="usb", SUBSYSTEM=="block", ACTION=="remove", ENV{ID_FS_LABEL}=="RPI-RP2", RUN+="${pkgs.writeShellScript "udev-rpi-unmount.sh" ''
+        ${pkgs.systemd}/bin/systemd-umount "$DEVNAME" "/run/media/$ID_FS_LABEL"
+        ${pkgs.coreutils}/bin/rmdir "/run/media/$ID_FS_LABEL"
+      ''}"
+    '';
+
     environment.systemPackages = with pkgs; [
       # Hyprland
-      pyprland        # Additional Hyprland Plugins
-      hyprpicker      # Color Picker
-      hyprcursor      # Cursor themes
-      hyprlock        # Screen Locker
-      hypridle        # Idle Management Daemon
-      hyprpaper       # Wallpaper Daemon
+      pyprland # Additional Hyprland Plugins
+      hyprpicker # Color Picker
+      hyprcursor # Cursor themes
+      hyprlock # Screen Locker
+      hypridle # Idle Management Daemon
+      hyprpaper # Wallpaper Daemon
 
       # General System Stuff
-      btop            # See System Health
-      psi-notify      # System Resource Alerter
-      poweralertd     # Power Alerter
-      playerctl       # Audio Control
-      waybar          # Bar
-      dunst           # Notification
-      rofi-wayland    # Launcher
-      avizo           # OSD (on-screen display) aka notification daemon
-      xfce.thunar     # File Manager
-      mpv             # Media Player
-      imv             # Image Viewer
-      zathura         # PDF Viewer
-      swappy          # Image Editor
-      grim            # Screenshot
-      slurp           # Screenshot
-      wl-screenrec    # Recording
-      wl-clipboard    # Clipboard
-      cliphist        # Clipboard
+      btop # See System Health
+      psi-notify # System Resource Alerter
+      poweralertd # Power Alerter
+      playerctl # Audio Control
+      waybar # Bar
+      dunst # Notification
+      rofi-wayland # Launcher
+      avizo # OSD (on-screen display) aka notification daemon
+      xfce.thunar # File Manager
+      mpv # Media Player
+      imv # Image Viewer
+      zathura # PDF Viewer
+      swappy # Image Editor
+      grim # Screenshot
+      slurp # Screenshot
+      wl-screenrec # Recording
+      wl-clipboard # Clipboard
+      cliphist # Clipboard
       wl-clip-persist # Clipboard
-      wlogout         # Logout menu
-      psmisc          # Terminal utils (used in some scripts)
-      wlrctl          # Terminal utils (just really cool, good potential scripting here)
-      wtype           # Terminal util  (used in some scripts)
-      ffmpeg_6-full   # Terminal util  (used in recording script)
+      wlogout # Logout menu
+      psmisc # Terminal utils (used in some scripts)
+      wlrctl # Terminal utils (just really cool, good potential scripting here)
+      wtype # Terminal util  (used in some scripts)
+      ffmpeg_6-full # Terminal util  (used in recording script)
 
       at-spi2-atk
       qt6.qtwayland
     ];
 
     # Security
-    security.pam.services.hyprlock = {};
+    security.pam.services.hyprlock = { };
   };
 }
