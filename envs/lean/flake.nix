1504da6afbd1f180aed70efee24a314486211bc6
Axel Sorenson <AxelPSorenson@gmail.com>
2026-01-20 17:12:55 -0700

    Lean env update

--- envs/lean/flake.nix
+++ envs/lean/flake.nix
@@ -1,28 +1,31 @@
 {
-  description = "Rust Environment";
+  description = "Lean Environment";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {
-    nixpkgs,
-    flake-utils,
-    ...
-  }:
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
     flake-utils.lib.eachDefaultSystem (
-      system: let
+      system:
+      let
         pkgs = import nixpkgs {
           inherit system;
         };
       in
-        with pkgs; {
-          devShells.default = mkShell {
-            buildInputs = with pkgs; [
-              lean4
-            ];
-          };
-        }
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = with pkgs; [
+            lean4
+          ];
+        };
+      }
     );
 }
