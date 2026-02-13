ffcf3f0a76758056b3acb80ff83f29b2a4280932
Axel Sorenson <AxelPSorenson@gmail.com>
2026-02-12 18:28:14 -0700

    Amber lang env

--- envs/amber/flake.nix
+++ envs/amber/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Amber dev environment with amber-lang and amber-lsp";
+  description = "amber-lang and amber-lsp";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -21,10 +21,50 @@
             owner = "amber-lang";
             repo = "amber-lsp";
             rev = "v0.1.16";
-            sha256 = "sha256-gozS+ty3vrbBL+biAZRv5wE3nqIGa9hG8Kmz8fa5f+s=";
+            sha256 = "sha256-thQh+yShChds4S/2/rbxJGpRMqirCnC2dmx0StLJGmY=";
           };
 
-          cargoHash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+          cargoHash = "sha256-yS0m2FCyqNztJXKRWye/II7lOg2H/DTQWXScxz5vO8k=";
+
+          postPatch = ''
+            substituteInPlace crates/analysis/src/stdlib.rs \
+              --replace 'fn get_stdlib_dir(amber_version: AmberVersion) -> Result<PathBuf, std::io::Error> {
+    let amber_subdir = match amber_version {
+        AmberVersion::Alpha034 => "alpha034",
+        AmberVersion::Alpha035 => "alpha035",
+        AmberVersion::Alpha040 => "alpha040",
+        AmberVersion::Alpha050 => "alpha050",
+    };
+
+    Ok(current_exe()?
+        .parent()
+        .unwrap()
+        .to_path_buf()
+        .join("amber-lsp-resources")
+        .join(amber_subdir))
+}' \
+          'fn get_stdlib_dir(amber_version: AmberVersion) -> Result<PathBuf, std::io::Error> {
+                let amber_subdir = match amber_version {
+                    AmberVersion::Alpha034 => "alpha034",
+                    AmberVersion::Alpha035 => "alpha035",
+                    AmberVersion::Alpha040 => "alpha040",
+                    AmberVersion::Alpha050 => "alpha050",
+                };
+
+                let base = std::env::var_os("XDG_DATA_HOME")
+                    .map(std::path::PathBuf::from)
+                    .unwrap_or_else(|| {
+                        std::env::var_os("HOME")
+                            .map(|h| std::path::PathBuf::from(h).join(".local/share"))
+                            .unwrap_or_else(|| std::path::PathBuf::from("."))
+                    })
+                    .join("amber-lsp")
+                    .join("amber-lsp-resources");
+
+                Ok(base.join(amber_subdir))
+            }';
+          '';
+
 
           meta = {
             description = "Amber Language Server";
