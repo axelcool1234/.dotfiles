67ed0dda1d3b304ddd048cd06135e9187f59e5c2
Axel Sorenson <AxelPSorenson@gmail.com>
2026-04-02 08:21:36 -0600

    Fix bluetooth

--- modules/features/bluetooth.nix
+++ modules/features/bluetooth.nix
@@ -1,8 +1,19 @@
-{ lib, ... }:
+{ lib, pkgs, ... }:
 {
   hardware.bluetooth = {
     enable = true;
-    powerOnBoot = false;
+    powerOnBoot = true;
+  };
+
+  systemd.services.bluetooth-unblock = {
+    description = "Unblock Bluetooth rfkill before BlueZ starts";
+    after = [ "systemd-rfkill.service" ];
+    before = [ "bluetooth.service" ];
+    wantedBy = [ "bluetooth.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${lib.getExe' pkgs.util-linux "rfkill"} unblock bluetooth";
+    };
   };
 
   preferences.impermanence.persist.systemDirectories = lib.mkAfter [
