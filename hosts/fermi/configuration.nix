3dd4f78c8a76a4db5f1bf2f2e8b331c2f6b3d44c
Axel Sorenson <AxelPSorenson@gmail.com>
2026-04-02 22:32:45 -0500

    Possible fix for Fermi page fault kernel panic

--- hosts/fermi/configuration.nix
+++ hosts/fermi/configuration.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }:
+{ inputs, pkgs, ... }:
 {
   imports = [
     # Include the results of the hardware scan.
@@ -11,5 +11,16 @@
     ./firewall.nix
     ./impermanence.nix
   ];
+
+  # Work around a VT regression in newer kernels that can panic in csi_J while
+  # greetd/tuigreet clears tty1 during login (black screen + hard freeze).
+  #
+  # Fermi hit this on 6.18.20 with a reproducible stack in do_con_write ->
+  # csi_J, so pin this host to the mature LTS line until the fix is fully
+  # backported in nixpkgs.
+  # https://www.spinics.net/lists/linux-serial/msg69608.html                                             
+  # https://bugzilla.kernel.org/show_bug.cgi?id=222168 
+  boot.kernelPackages = pkgs.linuxPackages_6_12;
+
   hardware.enableAllFirmware = true;
 }
